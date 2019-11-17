60293f1ab68e1d5ab093b351bdb8e3de2ec72407
hino <sou.guilty.ky@gmail.com>
2019-11-17 15:12:36 +0900

    Initial Commit

---
+++ Assets/2DSpaceshipsFreeTrial/Prefabs/2DScifiFighterWeapons/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 2f;
+    public Vector2 moveDirection = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector2(0,speed);
+
+        transform.Translate( moveDirection.x,moveDirection.y,0);
+    }
+    void OnBecameInvisible() {
+        Destroy (this.gameObject);
+    }
+}
---
+++ Assets/playerbeam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerbeam : MonoBehaviour
+{
+    public float speed = 2;
+
+    void OnBecameInvisible() 
+    {
+        Destroy (this.gameObject);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate (speed, 0, 0);
+    }
+}
