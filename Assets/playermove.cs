3510c48526b983d15f8063a1d8c9895ed1391e83
hino <sou.guilty.ky@gmail.com>
2019-12-07 08:24:27 +0900

    射撃ボタン押しっぱなしで連射するように変更

--- Assets/playermove.cs
+++ Assets/playermove.cs
@@ -7,6 +7,8 @@ public class playermove : MonoBehaviour
     // 移動スピード
     public float speed = 5;
     public GameObject leaserprefab;
+    public float timeOut;
+    private float timeElapsed;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,8 @@ public class playermove : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
+        timeElapsed += Time.deltaTime;
+
         // 右・左
         float x = Input.GetAxisRaw ("Horizontal");
     
@@ -29,8 +33,12 @@ public class playermove : MonoBehaviour
         // 移動する向きとスピードを代入する
         GetComponent<Rigidbody2D>().velocity = direction * speed;
 
-        if (Input.GetKeyDown (KeyCode.Space)) {
-			Instantiate (leaserprefab, transform.position, Quaternion.identity);
+        if (Input.GetKey (KeyCode.Space)) {
+            Vector2 tmp = GameObject.Find("2DFighterMinigun2").transform.position;
+            if (timeElapsed >= timeOut ){
+                Instantiate (leaserprefab, tmp, transform.rotation);
+                timeElapsed = 0;
+            }
         }
     }
 }
